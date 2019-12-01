9f885cc28d63891b4ba995d69d1fc1c578c9cbe1
Dirk Leifeld <leifeld@posteo.de>
2019-12-01 20:24:12 +0100

    format with rustfmt

--- src/day1.rs
+++ src/day1.rs
@@ -10,9 +10,10 @@ fn test_required_fuel() {
     assert_eq!(required_fuel(100756), 33583);
 }
 
-pub fn calculate_fuel_requirements() -> i64 {
+pub fn fuel_requirements() -> i64 {
     let input = include_str!("../static/day1_modules.txt");
-    input.lines()
+    input
+        .lines()
         .flat_map(str::parse::<i64>)
         .map(required_fuel)
         .sum()
@@ -20,23 +21,23 @@ pub fn calculate_fuel_requirements() -> i64 {
 
 fn fuel_for_fuel(mass: i64) -> i64 {
     let fuel = required_fuel(mass);
-    if fuel > 0{
+    if fuel > 0 {
         return fuel + fuel_for_fuel(fuel);
     }
     0
 }
 
 #[test]
-fn test_fuel_for_fuel()
-{
+fn test_fuel_for_fuel() {
     assert_eq!(fuel_for_fuel(14), 2);
     assert_eq!(fuel_for_fuel(1969), 966);
     assert_eq!(fuel_for_fuel(100756), 50346);
 }
 
-pub fn calculate_correct_fuel_requirements() -> i64 {
+pub fn correct_fuel_requirements() -> i64 {
     let input = include_str!("../static/day1_modules.txt");
-    input.lines()
+    input
+        .lines()
         .flat_map(str::parse::<i64>)
         .map(fuel_for_fuel)
         .sum()
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod day1;
 
 fn main() {
     println!("Hello, world!");
-    println!("fuel required {}", day1::calculate_fuel_requirements());
-    println!("correct fuel {}", day1::calculate_correct_fuel_requirements());
+    println!("fuel required {}", day1::fuel_requirements());
+    println!("correct fuel {}", day1::correct_fuel_requirements());
 }
