52fc9b0f531db90ba0596ecaabd7ec79c4a495fd
Dirk Leifeld <leifeld@posteo.de>
2019-12-02 17:50:47 +0100

    action test

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     println!("Hello, world!");
     println!("fuel required {}", day1::fuel_requirements());
     println!("correct fuel {}", day1::correct_fuel_requirements());
+
+    test
 }
